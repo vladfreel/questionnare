bd91870a908ccd206faf30aa7874a1a49d9195aa
vladfreel <vladfreelmaster@gmail.com>
2019-02-13 15:00:24 +0200

    add first test on question request

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@ require 'database_cleaner'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
+Dir[File.dirname(__FILE__) + "/support/*.rb"].each {|f| require f }
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
--- spec/requests/api/v1/questions_spec.rb
+++ spec/requests/api/v1/questions_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Questions API' do
+  it 'sends a list of questions' do
+    create(:question)
+    create(:question)
+    create(:question)
 
+    get '/api/v1/questions'
+
+    json = JSON.parse(response.body)
+
+    # test for the 200 status-code
+    expect(response).to be_success
+    # check to make sure the right amount of messages are returned
+    expect(json.count).to eq(3)
+  end
 end
